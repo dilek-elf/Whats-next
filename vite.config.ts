ceb9743dc1519874afc53d8f836182342934e421
Dilek-Elf <dilekelf11@gmail.com>
2026-07-17 16:30:55 +0200

    ci: deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Whats-next/',
   plugins: [tailwindcss(), react()],
 })
